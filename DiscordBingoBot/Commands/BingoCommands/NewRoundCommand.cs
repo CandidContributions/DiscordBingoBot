7ea144b060157f10abd0dc45f6e8faecb796ae57
Sven Geusens <sven@umbrace.be>
2020-05-30 22:10:49 +0200

    Finetuned bingoCard formatting

--- DiscordBingoBot/Commands/BingoCommands/NewRoundCommand.cs
+++ DiscordBingoBot/Commands/BingoCommands/NewRoundCommand.cs
@@ -23,14 +23,14 @@ namespace DiscordBingoBot.Commands.BingoCommands
             _permissionHandler = permissionHandler;
         }
 
-        [Command("newRound")]
+        [Command("newRound",RunMode = RunMode.Async)]
         [Summary("Starts a new round in the active bingo game")]
         public async Task NewRound()
         {
             await ExecuteNewRound();
         }
 
-        [Command("newRound")]
+        [Command("newRound", RunMode = RunMode.Async)]
         [Summary("Starts a new round in the active bingo game")]
         public async Task NewRoundVerbose(
             [Summary("Mode (verbose|silent)")]string mode)
@@ -52,6 +52,8 @@ namespace DiscordBingoBot.Commands.BingoCommands
 
         private async Task ExecuteNewRound(bool verbose = true)
         {
+            const char gridSeperator = '*';
+
             if (_permissionHandler.HasBingoManagementPermissions(Context) == false)
             {
                 return;
@@ -74,18 +76,28 @@ namespace DiscordBingoBot.Commands.BingoCommands
                     }
 
                     var stringBuilder = new StringBuilder();
-                    stringBuilder.AppendLine("Your new card: " + player.Grid.GridId + "\n");
+                    stringBuilder.AppendLine("Your new card: " + player.Grid.GridId);
+                    stringBuilder.AppendLine();
 
-                    stringBuilder.AppendLine("```=".PadRight(150, '=') + "\n");
+                    var longestWordLength = player.Grid.Rows.SelectMany(r => r.Items).Max(i => i.Length);
+                    var cellWidth = longestWordLength + 2;
+                    var gridWith = cellWidth * player.Grid.Rows.Length + player.Grid.Rows.Length;
+                    stringBuilder.AppendLine("```" + "".PadRight(gridWith + 1, gridSeperator));
                     for (var index = 0; index < player.Grid.Rows.Length; index++)
                     {
                         //stringBuilder.AppendLine(string.Join(" | ", player.Grid.Rows[index].Items.Select(w => w.PadRight(25))));
-                        const int pad = -20;
-
-                        stringBuilder.AppendLine(
-                            $"{player.Grid.Rows[index].Items[0],pad}\t\t\t{player.Grid.Rows[index].Items[1],pad}\t\t\t{player.Grid.Rows[index].Items[2],pad}\t\t\t{player.Grid.Rows[index].Items[3],pad}\t\t\t{player.Grid.Rows[index].Items[4],pad}\n");
+                        stringBuilder.Append(gridSeperator);
+                        foreach (var item in player.Grid.Rows[index].Items)
+                        {
+                            var leftPad = (cellWidth - item.Length) / 2;
+                            stringBuilder.Append("".PadRight(leftPad, ' ') + item +
+                                                 "".PadRight(cellWidth - item.Length - leftPad, ' ') + gridSeperator);
+                            
+                        }
+                        stringBuilder.AppendLine();
+                        stringBuilder.AppendLine("".PadRight(gridWith +1, gridSeperator));
                     }
-                    stringBuilder.AppendLine("=".PadRight(150, '=') + "```\n");
+                    stringBuilder.Append("```");
 
                     await playerSocket.SendMessageAsync(stringBuilder.ToString());
                 }
